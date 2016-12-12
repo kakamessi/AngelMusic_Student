f4ead6068c21c94a9f0c30400689d9aa147ba111
Kofeyseven <xie_longfei@126.com>
2016-12-12 14:34:44 +0800

    添加okhttp框架

--- app/src/main/java/com/angelmusic/student/base/BaseActivity.java
+++ app/src/main/java/com/angelmusic/student/base/BaseActivity.java
@@ -1,6 +1,5 @@
 package com.angelmusic.student.base;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -8,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-
 
 import com.angelmusic.student.core.ActionDispatcher;
 import com.angelmusic.student.utils.LogUtil;
---
+++ okhttp3/src/main/java/com/okhttplib/interceptor/ResultInterceptor.java
@@ -0,0 +1,12 @@
+package com.okhttplib.interceptor;
+
+import com.okhttplib.HttpInfo;
+
+/**
+ * 请求结果拦截器
+ */
+public interface ResultInterceptor {
+
+    HttpInfo intercept(HttpInfo info) throws Exception;
+
+}
