03fcf7e498c6b1a68c88828fe939579d525ad176
Kofeyseven <xieie_longfei@126.com>
2016-12-16 12:00:41 +0800

    修改okhttp3

--- app/src/main/java/com/angelmusic/student/version_update/AppUpdateManager.java
+++ app/src/main/java/com/angelmusic/student/version_update/AppUpdateManager.java
@@ -2,8 +2,6 @@ package com.angelmusic.student.version_update;
 
 import android.icu.util.VersionInfo;
 
-import com.okhttplib.HttpInfo;
-import com.okhttplib.OkHttpUtil;
 
 /**
  * Created by fei on 2016/12/13.
@@ -28,21 +26,21 @@ public class AppUpdateManager {
      * 请求网络检查是否需要更新
      */
     public static void checkVersion() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                HttpInfo info = HttpInfo.Builder()
-                        .setUrl(url)
-                        .addHead("head", "test")
-                        .build();
-                OkHttpUtil.getDefault(this)
-                        .doGetSync(info);
-                if (info.isSuccessful()) {
-                    final String result = info.getRetDetail();
-                    //进行Json解析
-
-                }
-            }
-        }).start();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                HttpInfo info = HttpInfo.Builder()
+//                        .setUrl(url)
+//                        .addHead("head", "test")
+//                        .build();
+//                OkHttpUtil.getDefault(this)
+//                        .doGetSync(info);
+//                if (info.isSuccessful()) {
+//                    final String result = info.getRetDetail();
+//                    //进行Json解析
+//
+//                }
+//            }
+//        }).start();
     }
 }
