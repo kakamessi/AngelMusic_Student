03584c25e72dd18b168d42b2b4d8381326aac032
Kofeyseven <xieie_longfei@126.com>
2016-12-16 12:05:00 +0800

    无修改

---
+++ okhttp3/src/main/java/com/okhttplib/annotation/RequestMethod.java
@@ -0,0 +1,16 @@
+package com.okhttplib.annotation;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 请求方法
+ */
+@IntDef({RequestMethod.POST, RequestMethod.GET})
+@Retention(RetentionPolicy.SOURCE)
+public @interface RequestMethod {
+    int POST = 1;
+    int GET = 2;
+}
