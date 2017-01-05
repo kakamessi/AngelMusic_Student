712a2e8d041d8011a3fd8e86b48033cbadc8406e
kakamessi <360895713@qq.com>
2017-01-05 10:19:47 +0800

    增加常规课界面

---
+++ app/src/main/java/com/angelmusic/student/activity/IdleActivity.java
@@ -0,0 +1,21 @@
+package com.angelmusic.student.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.angelmusic.student.R;
+
+public class IdleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idle);
+
+
+
+    }
+
+
+
+}
