f475d9efbec7cf325d6e7178b3c0da5bd80454e5
kakamessi <360895713@qq.com>
2017-04-20 08:42:04 +0800

    no message

--- app/src/main/java/com/angelmusic/student/activity/VideoActivity.java
+++ app/src/main/java/com/angelmusic/student/activity/VideoActivity.java
@@ -693,13 +693,13 @@ public class VideoActivity extends BaseActivity {
                 }else if(course_id == Constant.COURSE_1140 || course_id == Constant.COURSE_1196){
                     //C5
                     replaceLayout(llYuepu,R.layout.layout_yuepu_418c5_1);
-                    setNoteAndKey(llYuepu,1,true,MusicNote.getKeyIndex(36),false);
+                    setNoteAndKey(llYuepu,1,false,MusicNote.getKeyIndex(36),false);
                     MusicNote.openLight(VideoActivity.this,36,false);
 
                 }else if(course_id == Constant.COURSE_1180 || course_id == Constant.COURSE_1237){
                     //C6
                     replaceLayout(llYuepu,R.layout.layout_yuepu_418c6_1);
-                    setNoteAndKey(llYuepu,1,true,MusicNote.getKeyIndex(39),false);
+                    setNoteAndKey(llYuepu,1,false,MusicNote.getKeyIndex(39),false);
                     MusicNote.openLight(VideoActivity.this,39,false);
 
                 }else if(course_id == Constant.COURSE_1219 || course_id == Constant.COURSE_1277){
